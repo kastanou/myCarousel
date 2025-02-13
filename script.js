02a5465d89ce0ef3a6d2dd92525c4dc8b59fe44a
PetrosMelachroinidis <149314962+PetrosMelachroinidis@users.noreply.github.com>
2025-02-13 18:41:49 +0200

    Review

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const slideImages = document.querySelectorAll("img");
+const slideImages = document.querySelectorAll("img");  //FIXME: not a good practice as if you add a new image in the page you will include this in your carousel even if it is anywhere in the page. Try to group them with a class
 
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
@@ -11,10 +11,10 @@ let myInterval;
 
 const defaultTimer = 3000;
 
-stopButton = document.getElementById("stopButton");
-startButton = document.getElementById("startButton");
+stopButton = document.getElementById("stopButton");   //FIXME: where is const let?
+startButton = document.getElementById("startButton"); //FIXME: where is const let?
 
-submitButton = document.getElementById("submitButton");
+submitButton = document.getElementById("submitButton"); //FIXME: where is const let?
 
 const timerInput = document.getElementById("timerInput");
 
@@ -38,13 +38,14 @@ function slideNext() {
 prev.addEventListener("click", slidePrev);
 
 function slidePrev() {
-  slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
-  if (counter == 0) {
+  slideImages[counter].style.animation = "prev1 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
+  if (counter >= slideImages.length - 1) {
+  if (counter == 0) {  //FIXME: always use === instead of ==
     counter = slideImages.length - 1;
   } else {
     counter--;
   }
-  slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+  slideImages[counter].style.animation = "prev2 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
   indicators();
 }
 
@@ -63,22 +64,22 @@ function switchImage(currentImage) {
   currentImage.classList.add("active");
   let imageId = currentImage.getAttribute("attr");
   if (imageId > counter) {
-    slideImages[counter].style.animation = "next1 0.5s ease-in forwards";
+    slideImages[counter].style.animation = "next1 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
     counter = imageId;
-    slideImages[counter].style.animation = "next2 0.5s ease-in forwards";
+    slideImages[counter].style.animation = "next2 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
   } else if (imageId === counter) {
     return;
   } else {
-    slideImages[counter].style.animation = "prev1 0.5s ease-in forwards";
+    slideImages[counter].style.animation = "prev1 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
     counter = imageId;
-    slideImages[counter].style.animation = "prev2 0.5s ease-in forwards";
+    slideImages[counter].style.animation = "prev2 0.5s ease-in forwards"; //FIXME: try to add the css transtion logic inside the class and toogle with add remove class from classList. [Separation of concerns Princical]
   }
   indicators();
 }
 
 //Start button
 
-startButton.addEventListener("click", startInterval());
+startButton.addEventListener("click", startInterval());  //FIXME:  inside addEventListener we add function not the result of it so you need you need to add startInterval instead of startInterval()
 
 function startInterval(timer) {
   if (!myInterval) {
@@ -93,7 +94,7 @@ stopButton.addEventListener("click", stopInterval);
 function stopInterval() {
   if (myInterval) {
     slideImages.forEach((image) => {
-      image.removeAttribute("style");
+      image.removeAttribute("style"); //FIXME: again as mentioned above try to play with a specific class instead of style. There is a chance that in the applicaiton you may add a style in the future. By removing all the attribute you will face a bug as you will remove all the styles
     });
 
     clearInterval(myInterval);
